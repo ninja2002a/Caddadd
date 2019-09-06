86c9d2f0c9b426305b83b9fb9d87858492e6c0b9
ninja2002a <52730345+ninja2002a@users.noreply.github.com>
2019-09-06 10:48:32 -0700

    Add files via upload

---
+++ GuessingGame.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+char str[100];
+cin.get(str, 100);
+char input;
+
+cout << "Guess a number between 0-100 ";
+if (input > str) {
+std:cout << "To high\n";
+}
+else if (input < str) {
+std::cout << "To low\n";
+}
+
+
+ int rand() {
+  //Randomizes the numbers
+if (str == true) {
+ std:cout << "Nice\n";
+}
+   }
+   
+int srand()
+{
+
+   
+   }
+}
