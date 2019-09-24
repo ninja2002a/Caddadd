65876b69fd0aee308d1b9bfc99e2212e134bb47c
ninja2002a <52730345+ninja2002a@users.noreply.github.com>
2019-09-24 10:48:47 -0700

    Add files via upload

---
+++ Paul.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+//alt + x + linum-mode
+//palidrome is a thing that does thing, example: doggod = palidrome, apple = not palidrome
+//No more words than 80
+
+int main() {
+
+char str[80];
+ cin.get(
+char input;
+
+
+ cout << str << endl;
+
+if (input == " " || input.equals("")) {
+ }
+
+//checks for punctuations and spaces
+for (int i = 0; i < sizeof(str); i++) {
+  
+ }
+ 
+ return 0;
+}
