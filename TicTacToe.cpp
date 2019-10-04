182b94d896564dad732e7aa7410cf058636387ca
ninja2002a <52730345+ninja2002a@users.noreply.github.com>
2019-10-04 10:48:53 -0700

    Add files via upload

---
+++ TicTacToe.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+//alt + x + linum-mode
+
+int main() {
+
+  int board[3][3];
+  int CLEAR = 0;
+  int playerA = 1;
+  int playerB = 2;
+  int Aturn;
+  int Bturn;
+
+  //creates board
+  for (int board = 0; sizeof(board; ) {
+
+  }
+  return 0;
+}
