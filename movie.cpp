3da03fde8970276aebaaf5d2ca532d0ada9b2867
ninja2002a <52730345+ninja2002a@users.noreply.github.com>
2019-11-19 23:52:36 -0800

    Add files via upload

---
+++ movie.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+class mom {
+public:
+  //stores the data you added into movies
+  char* title;
+  char* director;
+  char* year;
+  char* length;
+  char* rate;
+};
