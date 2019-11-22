5970704bef6419c0591390e18affd47c6ac3fc31
ninja2002a <52730345+ninja2002a@users.noreply.github.com>
2019-11-22 10:50:09 -0800

    Add files via upload

---
+++ movie.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <cstring>
+#include "movie.h"
+
+movie::movie() {
+  
+};
---
+++ music.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <vector>
+#include "music.h"
+
+music::music() {
+  
+}
