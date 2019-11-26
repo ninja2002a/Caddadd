4d68f6febac932d316111d99af9375e067ededef
ninja2002a <52730345+ninja2002a@users.noreply.github.com>
2019-11-26 10:46:53 -0800

    Add files via upload

---
+++ zuul.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+//alt + x + linum-mode
+
+int main() {
+  char input = new char[30];
+
+  cout << "You are a wasp, you're goal is to sting people and bully insects because you're a wasp.";
+  cout << "Commands: Fly, Sting, Bite";
+}
