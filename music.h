2af93b83945f42563dcad264e8a282698f3d136f
ninja2002a <52730345+ninja2002a@users.noreply.github.com>
2019-11-25 22:40:57 -0800

    Add files via upload

---
+++ music.h
@@ -0,0 +1,16 @@
+#ifndef MUSIC_H
+#define MUSIC_H
+
+class music {
+ public:
+  //stores the data you added into music
+  char* title;
+  char* artist;
+  char* year;
+  char* duration;
+  char* publisher;
+  char* rating;
+  music();
+};
+
+#endif
---
+++ rad.h
@@ -0,0 +1,14 @@
+#ifndef RAD_H
+#define RAD_H
+
+class rad {
+public:
+  //stores the data you added into video games
+  char* title;
+  char* year;
+  char* publisher;
+  char* rate;
+  rad();
+};
+
+#endif
