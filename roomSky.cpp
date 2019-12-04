fbdcf5c2047f1313c8c235a07829360c4efa5665
ninja2002a <52730345+ninja2002a@users.noreply.github.com>
2019-12-04 00:28:19 -0800

    Add files via upload

---
+++ roomSky.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+#include "zuul.cpp"
+
+using namespace std;
+
+class Sky() {
+ public:
+  if (player == zuul.NO) {
+    cout << "You are at the sky.";
+ }
+  Sky();
+};
