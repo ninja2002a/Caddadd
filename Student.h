9cad3aefbdd55e025220f640235a3eceb753fa76
ninja2002a <52730345+ninja2002a@users.noreply.github.com>
2020-01-13 10:47:57 -0800

    Add files via upload

---
+++ Student.h
@@ -0,0 +1,21 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+class Student {
+ public:
+  Student* getFName;
+  Student* setFName;
+  Student* getLname;
+  Student* setLname;
+  Student* getID;
+  Student* setID;
+  Student* getGPA;
+  Student* setGPA;
+
+ private:
+  
+  
+  Student();
+};
+
+#endif
